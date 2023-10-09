ee401157491bbc4a6f1cbcd78ebff05f12d96955
Strzecha <matteuszdr@gmail.com>
2023-10-09 08:51:19 +0200

    Add comments to label.py

--- brownian/label.py
+++ brownian/label.py
@@ -2,10 +2,29 @@ from brownian.gui_component import GUIComponent
 from brownian.text import Text
 
 class Label(GUIComponent):
+    """Class Label
+    
+    Class to representation of label in interface
+    """
+
     def __init__(self, text, pos_x, pos_y):
+        """Init method
+        
+        Args:
+            text (str): text in label
+            pos_x (int): x position of label
+            pos_y (int): y position of label
+        """
+
         super().__init__()
 
         self.text = Text(text, (0, 0, 0), pos_x=pos_x, pos_y=pos_y)
 
     def draw(self, screen):
+        """Method to draw label on screen
+        
+        Args:
+            screen (pygame.display): screen
+        """
+        
         self.text.draw(screen)
