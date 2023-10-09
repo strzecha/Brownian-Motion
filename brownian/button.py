b39ed21759483e0695e99840595b8f6284b1ec9b
Strzecha <matteuszdr@gmail.com>
2023-10-09 08:38:39 +0200

    Add comments to button.py

--- brownian/button.py
+++ brownian/button.py
@@ -3,7 +3,25 @@ import pygame
 from brownian.gui_component import GUIComponent
 
 class Button(GUIComponent):
+    """Class Button
+    
+    Class to representation button in interface
+    """
+
     def __init__(self, width, height, pos_x, pos_y, text, action, color=(80, 80, 80), hover_color=(35, 35, 35)):
+        """Init method
+        
+        Args:
+            width (int): width of button
+            height (int): height of button
+            pos_x (int): x position of button
+            pos_y (int): y position of button
+            text (Text): text on button
+            action (function): action to perform when button is clicked
+            color (tuple): color of button
+            hover_color (tuple): hover color of button
+        """
+
         super().__init__()
 
         self.color = color
@@ -20,6 +38,9 @@ class Button(GUIComponent):
         self.set_text(text)
 
     def set_text(self, text):
+        """Text setter
+        """
+        
         self.text = text
         text_width, text_height = text.text_rendered.get_size()
 
@@ -29,6 +50,9 @@ class Button(GUIComponent):
         text.set_pos(margin_x, margin_y)
 
     def update(self):
+        """Update method
+        """
+        
         self.mouse = pygame.mouse.get_pos()
 
         if self.rect.left <= self.mouse[0] <= self.rect.right and self.rect.top <= self.mouse[1] <= self.rect.bottom:
@@ -37,10 +61,19 @@ class Button(GUIComponent):
             self.image.fill(self.color)
 
     def click(self):
+        """Method to detect button clicked
+        """
+        
         if self.rect.collidepoint(self.mouse):
             self.action()
 
     def draw(self, screen):
+        """Method to draw button on screen
+        
+        Args:
+            screen (pygame.display): screen
+        """
+        
         screen.blit(self.image, self.rect)
         self.text.draw(screen)
  
