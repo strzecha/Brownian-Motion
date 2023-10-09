0a91561ebec87e9044d84ec648e5eaa0f1ec695b
Strzecha <matteuszdr@gmail.com>
2023-10-09 08:44:24 +0200

    Add comments to gui_component.py

--- brownian/gui_component.py
+++ brownian/gui_component.py
@@ -1,12 +1,29 @@
 import pygame
 
 class GUIComponent(pygame.sprite.Sprite):
+    """Class GUIComponent
+    
+    Class to representation component in interface
+    """
+
     def __init__(self):
+        """Init method
+        """
+        
         super().__init__()
 
     def handle_event(self, event):
+        """Method to handle event
+        
+        Args:
+            event (pygame.event): event
+        """
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             self.click()
 
     def click(self):
+        """Method to detect component clicked
+        """
+        
         pass
